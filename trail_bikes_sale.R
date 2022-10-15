bb4b8361e80bf1c1bd543854c47412dbd9d924ea
Claudia Penaloza <claudia.penaloza@conti-na.com>
2022-10-15 13:11:40 -0400

    Adds 5010c blue book.

--- trail_bikes_sale.R
+++ trail_bikes_sale.R
@@ -6,3 +6,4 @@ plot(qrcode::qr_code("https://www.facebook.com/photo/?fbid=10160136758521866&set
 plot(qrcode::qr_code("https://www.theproscloset.com/products/2018-santa-cruz-5010-c-s-l-2?variant=41816759894208&g_acctid=228-971-4576&g_adgroupid=142346740312&g_adid=624597117565&g_adtype=pla&g_campaign=US+-+Shopping+-+Standard+-+Bikes&g_campaignid=18448096476&g_ifcreative=&g_ifproduct=product&g_keyword=&g_keywordid=aud-1792316812311:pla-1794758422441&g_merchantid=123636776&g_network=g&g_partition=1794758422441&g_productchannel=online&g_productid=shopify_US_7165680681152_41816759894208&gclid=Cj0KCQjwnP-ZBhDiARIsAH3FSRdssnxCBk1dc-cCe_6Xbm_AmfY1-n_RAa2Q4AW07cZZT_L9DtEsxdgaAtc-EALw_wcB"))
 
 plot(qrcode::qr_code("https://www.facebook.com/photo/?fbid=10160136787326866&set=a.415530396865"))
+plot(qrcode::qr_code("https://www.bicyclebluebook.com/value-guide/product/3079756/"))
